75cf32f85a856d8efb3b00a9815564cdbb1ffa54
PBTech <pavelb.tech@gmail.com>
2020-09-11 17:54:53 +0300

    four

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import sys
+import pygame
+from settings import Settings
+
+
+def run_game():
+    # initialize the game
+    pygame.init()
+    fg_settings = Settings()
+    screen = pygame.display.set_mode((fg_settings.screen_width, fg_settings.screen_height))
+    pygame.display.set_caption('First Game')
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(fg_settings.bg_color)
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
-    print('Hello!')
+    run_game()
